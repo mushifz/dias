c365e6d25a1aebe284e8c9d03fcf3f331bf5595c
mushifz <20243rd062@utez.edu.mx>
2025-05-13 12:31:31 -0600

    Se agrego un inicio de sesion basico

--- CompararCadenas.java
+++ CompararCadenas.java
@@ -11,8 +11,28 @@ public class CompararCadenas {
         return numeroTemporal;
     }
 
+    public static String obtenerCadena(String mensaje){
+        String cadenaTemporal;
+        System.out.println(mensaje);
+        cadenaTemporal = scanner.nextLine();
+        return cadenaTemporal;
+    }
+
     public static void main(String[]args){
-        int numero = obtenerNumero("Ingresa un numero ");
+        /*int numero = obtenerNumero("Ingresa un numero ");
         System.out.println("El numero ingresado es: " + numero);
+        scanner.nextLine();
+        String cadena = obtenerCadena("Ingresa un mensaje ");
+        System.out.println("El mensaje ingresado es: " + cadena);*/
+
+        String userMail = "20243rd062@utez.edu.mx";
+        String userPass = "1234";
+        String mailUsuario = obtenerCadena("Ingresa tu nombre ");
+        String passUsuario = obtenerCadena("Ingresa tu contraseña ");
+        if (userMail.equals(mailUsuario) && userPass.equals(passUsuario)){
+            System.out.println("bienvenido ");
+        }else{
+            System.out.println("correo o contraseña incorrectos ");
+        }
     }
 }
